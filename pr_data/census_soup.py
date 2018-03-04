ea27b3e2eccfc6061fefb9daff832905be140e45
Lgsc <gsanchez@civisanalytics.com>
2018-03-04 16:53:50 -0600

    census coding

--- pr_data/census_soup.py
+++ pr_data/census_soup.py
@@ -1,9 +1,15 @@
-import urllib
 import urllib.request
 from bs4 import BeautifulSoup
+import urllib
 import pandas as pd
 import logging
+import time
+import os
+
 from pr_data.base import exports as exp
+
+
+log = logging.getLogger(__name__)
 
 LAT = '18.16608882'
 LONG = '-66.04229489'
@@ -45,6 +51,8 @@ def get_block_data(latitude, longitude):
     soup = make_soup(url)
 
     data_dict = dict()
+    data_dict['latitude'] = latitude
+    data_dict['longitude'] = longitude
     data_dict['state_name'] = find_state_name(soup)
     data_dict['county_name'] = find_county_name(soup)
     data_dict['state_code'] = find_state_code(soup)
@@ -53,23 +61,47 @@ def get_block_data(latitude, longitude):
     return data_dict
 
 
-def statename(latitude, longitude):
-    url=('https://geo.fcc.gov/api/census/' + 'block/find?' +
-        'latitude={lat}&longitude={long}'.format(lat=latitude, long=longitude))
-    soup = make_soup(url)
-    return find_block_fips(soup)
-
-
 def main():
     logging.basicConfig(level=logging.INFO)
 
     #dat = get_block_data(LAT, LONG)
     #logging.info(dat)
     raw_data = exp.get_df_from_csv(file_name='data/mapa_del_crimen.csv')
-    raw_data = raw_data[:10]
-    # raw_data['POINT_X']
-    raw_data['state_name'] = raw_data.apply(lambda x: statename(x['POINT_X'], x['POINT_Y']), axis=1)
-    print(raw_data['state_name'])
+    raw_data = raw_data[:10000]
+
+    # -----------
+    #  THIS NEED TO BE PULLED OUT AS A FUNCTION
+    list_of_data = list()
+    log_thresh = 100
+    inc = log_thresh
+    t0 = time.time()
+    for index, row in raw_data.iterrows():
+        # when to log
+        if (index + 1) == log_thresh:
+            now = time.time()
+            run_time = now-t0
+            log.info('Finished {count} Rows, Run Time: {time} seconds'.format(
+                count=log_thresh, time=round(run_time, 2)))
+            log_thresh += inc
+
+        try:
+            dat = get_block_data(latitude=row['POINT_X'], longitude=row['POINT_Y'])
+            # log.info(dat)
+            list_of_data.append(dat)
+        except:
+            logging.info('============= SKIPPED A ROW =============')
+            pass
+
+    t1 = time.time()
+    total = t1-t0
+    log.info('Total Run Time: {time} seconds'.format(time=round(total, 2)))
+
+    # ------
+
+    os.chdir('/Users/gsanchez/Desktop')
+    data = pd.DataFrame(list_of_data)
+    log.info('Writing {num} data point'.format(num=len(data)))
+    data.to_csv('geo_coded_crimes.csv')
 
 
 if __name__ == '__main__':
